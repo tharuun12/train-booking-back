e39092b5c419906e1b323c274978d9fcdeed0f37
tharuun <tharuun2003@gmail.com>
2024-12-30 22:39:24 +0530

    first commitcasdf

--- db.js
+++ db.js
@@ -1,18 +1,17 @@
 require('dotenv').config(); // Load environment variables from .env file
 const fs = require('fs');
-const path = require('path');
 
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'avnadmin',
-  host: 'pg-1986e413-tharuuncloud-2c82.g.aivencloud.com',
-  database: 'ticket',
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
   password: process.env.PG_PWD,  // Access the password from the environment variable
-  port: 25582,
+  port: process.env.PG_PORT,
   ssl: {
     rejectUnauthorized: true, // Ensure SSL is enforced
-    ca: fs.readFileSync(path.join(__dirname, 'ca.pem')) // Provide the path to the CA certificate
+    ca: fs.readFileSync(process.env.PG_CA_CERT) // Provide the path to the CA certificate
   }
 });
 
